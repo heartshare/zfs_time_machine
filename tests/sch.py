f33dba01593a329d372cc4b38d0ae9438e7d1992
Fly me to the moon <git@bigly.dog>
2021-04-05 00:17:30 -0700

    ok

--- tests/sch.py
+++ tests/sch.py
@@ -1,16 +1,33 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from unittest import TestCase
 
-from ..ztm.consts import WEEK
+from ..ztm.consts import DAY, HOUR, MINUTE, MONTH, WEEK
 from ..ztm.sch import keep, tabulate
 from ..ztm.types import Snaps
 
 
 class Tabulate(TestCase):
+    def setUp(self) -> None:
+        self.now = datetime.now(tz=timezone.utc)
+
     def test_1(self) -> None:
-        self.assertEqual(1, 1)
+        snapshots = {
+            self.now + MONTH,
+            self.now + WEEK,
+            self.now + DAY,
+            self.now + HOUR,
+            self.now + MINUTE,
+        }
+        snaps = tabulate(snapshots, now=self.now)
+        print(snaps)
 
 
 class Keep(TestCase):
     def test_1(self) -> None:
-        self.assertEqual(1, 1)
+        snaps = Snaps(
+            gt_month=set(),
+            day_month=set(),
+            hour_day=set(),
+            le_hour=set(),
+        )
+        self.assertEqual(snaps, snaps)
